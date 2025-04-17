805624ffb5b82030aa21fb139a5f6cae69ea7757
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-04-18 03:10:29 +0530

    App Layout And Outlet Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,43 @@
 import './App.css'
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Route, RouterProvider} from "react-router-dom";
 import Contact from './pages/Contact';
 import Movie from './pages/Movie';
 import About from './pages/About';
 import Home from './pages/Home';
+import AppLayout from './Components/layout/AppLayout';
 
 function App() {
-// const router=createBrowserRouter([
-//   {
-//     path:"/",
-//     element:<Home/>,
-//   },
-//   {
-//     path:"/about",
-//     element:<About/>,
-//   },
-//   {
-//     path:"/movie",
-//     element:<Movie/>,
-//   },
-//   {
-//     path:"/contact",
-//     element:<Contact/>,
-//   },
-// ])
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+        {
+          path:"/",
+          element:<Home/>,
+        },
+        {
+          path:"/about",
+          element:<About/>,
+        },
+        {
+          path:"/movie",
+          element:<Movie/>,
+        },
+        {
+          path:"/contact",
+          element:<Contact/>,
+        },
+      
+    ]
+  },
+  
+])
 
-//   return <RouterProvider router={router}/>
+  return <RouterProvider router={router}/>
 
 
-//Old Way:-
 
-createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home/>} />
-    <Route path="/about" element={<About/>}/>
-    <Route path="/contact" element={<Contact/>}/>
-    <Route path="/movie" element={<Movie/>}/>
-  </Route>
-
-)
 }
-
-
-
 
 export default App
