8fa326f8fa74d69dc2ef3ae05d61057db856718f
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-04-23 03:51:58 +0530

    Secured API key Using .env

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const getMoviesData = async () => {
     try {
-        const response = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=4c59a226&s=titanic&page=1");
+        const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}=titanic&page=1`);
         const data = await response.json();
         return data;
     } catch (error) {
